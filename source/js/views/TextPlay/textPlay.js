c801d68f1d5cc16b9883116d2a89a4411ae92b4a
Omkar Kulkarni_2 <omkar.kulkarni@sas.com>
2017-07-18 14:52:11 -0400

    Added draftjs library and a simple text Editor

--- source/js/views/TextPlay/textPlay.js
+++ source/js/views/TextPlay/textPlay.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Editor, EditorState} from 'draft-js';
-import {Form, TextArea} from 'semantic-ui-react';
+import {Editor, EditorState, DefaultDraftBlockRenderMap, RichUtils} from 'draft-js';
+import {Segment, Button} from 'semantic-ui-react';
+import { Map } from 'immutable';
 
 class textPlay extends React.Component {
 
@@ -14,18 +15,52 @@ class textPlay extends React.Component {
     this.onChange = this.onChange.bind(this);
   }
 
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+    if(newState){
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'non-handled';
+
+  };
+
   onChange(editorState){
     this.setState({
       editorState: editorState
     });
   }
 
+  onUnderlineClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  };
+
+  onBoldClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  };
+
+  onItalicClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  };
+
+
   render(){
     return  (
-      <Editor
-        placeHolder="Start writing something"
-        editorState={this.state.editorState}
-        onChange={(editorState) => {this.onChange(editorState)}} />
+      <div>
+      <Segment>
+        <Editor
+          handleKeyCommand={this.handleKeyCommand}
+          editorState={this.state.editorState}
+          onChange={(editorState) => {this.onChange(editorState)}} />
+      </Segment>
+        <Button.Group>
+          <Button icon='bold' onClick={this.onBoldClick} />
+          <Button icon='underline' onClick={this.onUnderlineClick} />
+          <Button icon='italic' onClick={this.onItalicClick}/>
+        </Button.Group>
+      </div>
     );
   }
 }
