55253fc062f2cbce21725ab21cbf594825fdf0d0
Omkar Kulkarni_2 <omkar.kulkarni@sas.com>
2017-07-20 12:41:26 -0400

    added keystroke for highlighting and listing. Next focus on creating sublists

--- source/js/views/TextPlay/textPlay.js
+++ source/js/views/TextPlay/textPlay.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Editor, EditorState, DefaultDraftBlockRenderMap, RichUtils} from 'draft-js';
+import {EditorState, DefaultDraftBlockRenderMap, RichUtils} from 'draft-js';
 import {Segment, Button} from 'semantic-ui-react';
 import { Map } from 'immutable';
+import Editor from 'draft-js-plugins-editor';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import 'draft-js-emoji-plugin/lib/plugin.css';
+
+//custom plugin
+import createHighlightPlugin from '../../editorPlugins/highlightPlugin';
+
+import createAutoListPlugin from 'draft-js-autolist-plugin';
+const autoListPlugin = createAutoListPlugin();
+
+//Create a new Instance of Plugins:
+const emojiPlugin = createEmojiPlugin();
+const { EmojiSuggestions } = emojiPlugin;
+const highLightPlugin = createHighlightPlugin();
 
 class textPlay extends React.Component {
-
   constructor(props){
     super(props);
     this.state = {
       editorState: EditorState.createEmpty()
     };
-
     this.onChange = this.onChange.bind(this);
   }
 
@@ -53,7 +65,9 @@ class textPlay extends React.Component {
         <Editor
           handleKeyCommand={this.handleKeyCommand}
           editorState={this.state.editorState}
-          onChange={(editorState) => {this.onChange(editorState)}} />
+          onChange={(editorState) => {this.onChange(editorState)}}
+          plugins={[emojiPlugin, highLightPlugin, autoListPlugin]}/>
+          <EmojiSuggestions/>
       </Segment>
         <Button.Group>
           <Button icon='bold' onClick={this.onBoldClick} />
